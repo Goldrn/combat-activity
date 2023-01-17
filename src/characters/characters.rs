d9e243e32c106ebf1f23bc79d76c8e604a5a2c97
Goldrn <goldrn@users.noreply.github.com>
2023-01-17 12:44:13 -0500

    removed usesless bullshit

--- src/characters/characters.rs
+++ src/characters/characters.rs
@@ -3,8 +3,6 @@ use crate::characters::characters::character_base::CharacterBase;
 
 pub fn make_characters() -> Vec<CharacterBase> {
     let mut charlist: Vec<CharacterBase> = Vec::new();
-    let jerry: CharacterBase;
-    let jill: CharacterBase;
     let jerry = CharacterBase { hp: 50, moves: CharacterBase::generate_moves("punch".parse().unwrap(), "kick".parse().unwrap(), "hpunch".parse().unwrap(), "hkick".parse().unwrap(), "spec".parse().unwrap()), name: ("jerry").parse().unwrap() };
     let jill = CharacterBase { hp: 70, moves: CharacterBase::generate_moves("punch".parse().unwrap(), "kick".parse().unwrap(), "hpunch".parse().unwrap(), "hkick".parse().unwrap(), "spec".parse().unwrap()), name: ("jill").parse().unwrap() };
     charlist.push(jerry);
